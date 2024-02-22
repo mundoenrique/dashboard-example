243694205df84a4dbf3fb886e2d78bb91cf5cef6
mundoenrique <enrique.penaloza.working@gmail.com>
2024-02-21 22:55:57 -0400

    Consultas a la base de datos

--- src/app/ui/dashboard/cards.tsx
+++ src/app/ui/dashboard/cards.tsx
@@ -1,9 +1,4 @@
-import {
-  BanknotesIcon,
-  ClockIcon,
-  UserGroupIcon,
-  InboxIcon,
-} from '@heroicons/react/24/outline';
+import { BanknotesIcon, ClockIcon, UserGroupIcon, InboxIcon } from '@heroicons/react/24/outline';
 import { lusitana } from '@/app/ui/fonts';
 
 const iconMap = {
@@ -21,11 +16,7 @@ export default async function CardWrapper() {
       {/* <Card title="Collected" value={totalPaidInvoices} type="collected" />
       <Card title="Pending" value={totalPendingInvoices} type="pending" />
       <Card title="Total Invoices" value={numberOfInvoices} type="invoices" />
-      <Card
-        title="Total Customers"
-        value={numberOfCustomers}
-        type="customers"
-      /> */}
+      <Card title="Total Customers" value={numberOfCustomers} type="customers" /> */}
     </>
   );
 }
