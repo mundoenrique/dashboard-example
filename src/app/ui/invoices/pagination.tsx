e3d84b86bd2394324d735cccf6ccd9ef652fb2be
mundoenrique <enrique.penaloza.working@gmail.com>
2024-02-23 08:49:54 -0400

    Busqueda y paginación

--- src/app/ui/invoices/pagination.tsx
+++ src/app/ui/invoices/pagination.tsx
@@ -4,11 +4,21 @@ import { ArrowLeftIcon, ArrowRightIcon } from '@heroicons/react/24/outline';
 import clsx from 'clsx';
 import Link from 'next/link';
 import { generatePagination } from '@/app/lib/utils';
+import { usePathname, useSearchParams } from 'next/navigation';
 
 export default function Pagination({ totalPages }: { totalPages: number }) {
   // NOTE: comment in this code when you get to this point in the course
 
   // const allPages = generatePagination(currentPage, totalPages);
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
+  const currentPage = Number(searchParams.get('page')) || 1;
+
+  const createPageURL = (pageNumber: number | string) => {
+    const params = new URLSearchParams(searchParams);
+    params.set('page', pageNumber.toString());
+    return `${pathname}?${params.toString()}`;
+  };
 
   return (
     <>
@@ -63,16 +73,13 @@ function PaginationNumber({
   position?: 'first' | 'last' | 'middle' | 'single';
   isActive: boolean;
 }) {
-  const className = clsx(
-    'flex h-10 w-10 items-center justify-center text-sm border',
-    {
-      'rounded-l-md': position === 'first' || position === 'single',
-      'rounded-r-md': position === 'last' || position === 'single',
-      'z-10 bg-blue-600 border-blue-600 text-white': isActive,
-      'hover:bg-gray-100': !isActive && position !== 'middle',
-      'text-gray-300': position === 'middle',
-    },
-  );
+  const className = clsx('flex h-10 w-10 items-center justify-center text-sm border', {
+    'rounded-l-md': position === 'first' || position === 'single',
+    'rounded-r-md': position === 'last' || position === 'single',
+    'z-10 bg-blue-600 border-blue-600 text-white': isActive,
+    'hover:bg-gray-100': !isActive && position !== 'middle',
+    'text-gray-300': position === 'middle',
+  });
 
   return isActive || position === 'middle' ? (
     <div className={className}>{page}</div>
@@ -92,22 +99,14 @@ function PaginationArrow({
   direction: 'left' | 'right';
   isDisabled?: boolean;
 }) {
-  const className = clsx(
-    'flex h-10 w-10 items-center justify-center rounded-md border',
-    {
-      'pointer-events-none text-gray-300': isDisabled,
-      'hover:bg-gray-100': !isDisabled,
-      'mr-2 md:mr-4': direction === 'left',
-      'ml-2 md:ml-4': direction === 'right',
-    },
-  );
+  const className = clsx('flex h-10 w-10 items-center justify-center rounded-md border', {
+    'pointer-events-none text-gray-300': isDisabled,
+    'hover:bg-gray-100': !isDisabled,
+    'mr-2 md:mr-4': direction === 'left',
+    'ml-2 md:ml-4': direction === 'right',
+  });
 
-  const icon =
-    direction === 'left' ? (
-      <ArrowLeftIcon className="w-4" />
-    ) : (
-      <ArrowRightIcon className="w-4" />
-    );
+  const icon = direction === 'left' ? <ArrowLeftIcon className="w-4" /> : <ArrowRightIcon className="w-4" />;
 
   return isDisabled ? (
     <div className={className}>{icon}</div>
